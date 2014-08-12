2691e6b80300388178a27805ccf9dbb6ced9a8ce
Kamil Hakim <kamil.hakim.swe@gmail.com>
2014-08-12 14:17:53 +0200

    changed the order of methods

--- lib/hiera/backend/http_backend.rb
+++ lib/hiera/backend/http_backend.rb
@@ -33,6 +33,57 @@ class Hiera
         end
       end
 
+      # Handlers
+      # Here we define specific handlers to parse the output of the http request
+      # and return a value. Currently we support YAML and JSON
+      #
+      def json_handler(key,answer)
+        require 'rubygems'
+        require 'json'
+        JSON.parse(answer)[key]
+      end
+
+      def yaml_handler(key,answer)
+        begin
+          require 'yaml'
+          y = YAML.load answer
+          y[key]
+        rescue Exception => e
+          $stderr.puts "failed to parse: #{e.message}"
+          $stderr.puts e.backtrack
+        end
+      end
+
+      def parse_response(key,answer)
+
+        return nil unless answer
+
+        Hiera.debug("[hiera-http]: Query returned data, parsing response as #{@config[:output] || 'plain'}")
+
+        case @config[:output]
+
+          when 'plain'
+            # When the output format is configured as plain we assume that if the
+            # endpoint URL returns an HTTP success then the contents of the response
+            # body is the value itself, or nil.
+            #
+            answer
+          when 'json'
+            # If JSON is specified as the output format, assume the output of the
+            # endpoint URL is a JSON document and return keypart that matched our
+            # lookup key
+            self.json_handler(key,answer)
+          when 'yaml'
+            # If YAML is specified as the output format, assume the output of the
+            # endpoint URL is a YAML document and return keypart that matched our
+            # lookup keyi
+            $stderr.puts 'yaml case'
+            self.yaml_handler(key,answer)
+          else
+            answer
+        end
+      end
+
       def lookup(key, scope, order_override, resolution_type)
 
         #stop execution if the key is not pre-configured.
@@ -87,58 +138,7 @@ class Hiera
         end
         answer
       end
-
-
-      def parse_response(key,answer)
-
-        return nil unless answer
-
-        Hiera.debug("[hiera-http]: Query returned data, parsing response as #{@config[:output] || 'plain'}")
-
-        case @config[:output]
-
-          when 'plain'
-            # When the output format is configured as plain we assume that if the
-            # endpoint URL returns an HTTP success then the contents of the response
-            # body is the value itself, or nil.
-            #
-            answer
-          when 'json'
-            # If JSON is specified as the output format, assume the output of the
-            # endpoint URL is a JSON document and return keypart that matched our
-            # lookup key
-            self.json_handler(key,answer)
-          when 'yaml'
-            # If YAML is specified as the output format, assume the output of the
-            # endpoint URL is a YAML document and return keypart that matched our
-            # lookup keyi
-            $stderr.puts 'yaml case'
-            self.yaml_handler(key,answer)
-          else
-            answer
-        end
-      end
-
-      # Handlers
-      # Here we define specific handlers to parse the output of the http request
-      # and return a value. Currently we support YAML and JSON
-      #
-      def json_handler(key,answer)
-        require 'rubygems'
-        require 'json'
-        JSON.parse(answer)[key]
-      end
-
-      def yaml_handler(key,answer)
-        begin
-          require 'yaml'
-          y = YAML.load answer
-          y[key]
-        rescue Exception => e
-          $stderr.puts "failed to parse: #{e.message}"
-          $stderr.puts e.backtrack
-        end
-      end
+      
     end
   end
 end
